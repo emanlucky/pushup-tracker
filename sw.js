2f86a21b802df53cc9a2ae1519c1894ad2b57dc3
emanlucky <163803902+emanlucky@users.noreply.github.com>
2026-08-09 21:58:29 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,58 @@
 const CACHE = "pushup-tracker-v2";
-const FILES = ["./", "./index.html", "./style.css", "./app.js", "./manifest.json", "./icon.svg"];
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(FILES);
+    })
+  );
+
   self.skipWaiting();
 });
 
+
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
 });
 
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+
+    caches.match(event.request)
+      .then(cached => {
+
+        return cached || fetch(event.request);
+
+      })
+
   );
+
 });
